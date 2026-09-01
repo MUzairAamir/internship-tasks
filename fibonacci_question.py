04831772456b4a8b52fb411b8b22c2fafc59d79f
Uzair <muhammaduzairaamir@gmail.com>
2026-09-01 13:04:26 +0500

    giving comments

--- fibonacci_question.py
+++ fibonacci_question.py
@@ -3,12 +3,12 @@ def factorial(number):
     return 0
   if number == 1:
     return 1
-
+     #simple multiplication of numbers to get the factorial of a number
   return number*factorial(number-1)
 def fibiconi(number):
-  if number <= 0:
+  if number <= 0: # resolving problem of 0 and negative fibonacci numbers
     return 0
-  if number == 1 or number == 2:
+  if number == 1 or number == 2:   # resolving problem of 1st and 2nd fibonacci numbers
     return 1
 
   return fibiconi(number-1) + fibiconi(number-2)
