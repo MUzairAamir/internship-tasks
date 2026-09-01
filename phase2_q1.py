16a6f38c7f81697692737295274e85206ea6baec
Uzair <muhammaduzairaamir@gmail.com>
2026-09-01 13:09:13 +0500

    comments added on phase2 question1

--- phase2_q1.py
+++ phase2_q1.py
@@ -4,12 +4,15 @@ class Shape:
     self.length =lenght
     self.width = width 
     
-  
+  # just a placeholder method to be overridden by subclasses
     
   def area(self):
     area = self.length * self.width
     return area;
-
+# there are two ways to implement the area method in the subclasses,#
+#  one is to use the super() method to call the parent class's area method and then modify it as needed,
+# # and the other is to completely override the area method in the subclass. In this case,
+# # we will override the area method in each subclass to provide the specific implementation for that shape.
 class Circle(Shape):
   def __init__(self,radius):
     
@@ -20,7 +23,7 @@ class Circle(Shape):
 class Rectangle(Shape):
   def __init__(self,length,width):
     super().__init__(length,width)
-  
+  # simply using the parent class's area method to calculate the area of the rectangle
   
 class Triangle(Shape):
   def __init__(self,length,width):
